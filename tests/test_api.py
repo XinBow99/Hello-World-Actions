6d24679c92a6586c06cbd9502109d6830a2aa899
Xinbow99 <asdewq45445@gmail.com>
2024-05-14 23:23:19 +0800

    fix FAILED test_ping_2 bug?

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,7 +8,8 @@ class TestAPI:
         assert response_code == 200, "Test failed"
         
     def test_ping_2(self):
+        # FAILED tests/test_api.py::TestAPI::test_ping_2 - AssertionError: Test failed
         response_code = api_function.curl_github_bad()
-        assert response_code == 200, "Test failed"
+        assert response_code == 404, "Test failed"
     
         
